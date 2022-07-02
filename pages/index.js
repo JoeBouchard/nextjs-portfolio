6575b6850e464e5bffcfe0627a71ae0cab205275
Joe Bouchard <joseph.c.bouchard-1@ou.edu>
2022-07-02 18:43:45 -0500

    Added github profile picture and icons

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,52 @@
 import Head from "next/head";
 import Link from "next/link";
+import React, { useState, useEffect } from "react";
+import {
+  FaPython as Py,
+  FaReact,
+  FaJs as Js,
+  FaJenkins as Jenkins,
+  FaLinux as Linux,
+} from "react-icons/fa";
+import { BsFillBootstrapFill as Bs } from "react-icons/bs";
+import { SiTailwindcss as Tailwind, SiNginx as Nginx } from "react-icons/si";
 
-export default function Home() {
+const Home = () => {
+  const [gitInfo, setGitInfo] = useState({});
+  useEffect(() => {
+    (async () => {
+      const resp = await fetch("https://api.github.com/users/JoeBouchard");
+      const data = await resp.json();
+      setGitInfo(data);
+    })();
+  }, []);
+
   return (
-    <div className="container">
+    <div className={`container`}>
       <Head>
-        <title>Create Next App</title>
+        <title>Joe Bouchard's Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">Welcome to Joe's Porfolio!</h1>
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-        <div className="grid">
+        <img
+          src={gitInfo.avatar_url}
+          alt="Profile picture"
+          width="20%"
+          className="h-36 w-36 mx-auto rounded-full border border-zinc-700 dark:border-zinc-400 border-4"
+        />
+        <h1 className="animate-fadeUp text-6xl font-bold text-center my-4">
+          Joseph Bouchard
+        </h1>
+        <h3 className="animate-fadeUp text-2xl text-center my-4">
+          Fullstack Developer for the Oklahoma Department of Public Safety
+        </h3>
+        <h3 className="w-64 mx-auto">
+          <div className="text-3xl text-center my-4 grid grid-cols-7 gap-4">
+            <FaReact /> <Js /> <Py /> <Bs /> <Tailwind /> <Linux /> <Nginx />
+          </div>
+        </h3>
+        <div className="grid grid-cols-2 grid-flow-col gap-4">
           <a href="https://nextjs.org/docs" className="card">
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
@@ -46,162 +78,8 @@ export default function Home() {
           </a>
         </div>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   );
-}
+};
+
+export default Home;
