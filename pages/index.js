596932c7e8d063a642124183b67ba1bd34624467
Joe Bouchard <joseph.c.bouchard-1@ou.edu>
2022-07-02 16:46:58 -0500

    First pass

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,24 +10,22 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
+        <h1 className="title">Welcome to Joe's Porfolio!</h1>
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
-
         <div className="grid">
           <a href="https://nextjs.org/docs" className="card">
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <div className="card">
+            <h3>Projects &rarr;</h3>{" "}
+            <p>
+              <Link href="/projects/rims">Learn about Joe's projects!</Link>
+            </p>
+          </div>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
@@ -54,8 +53,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
+          Powered by <img src="/vercel.svg" alt="Vercel" className="logo" />
         </a>
       </footer>
 
@@ -205,5 +203,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
---
+++ pages/projects/rims.js
@@ -0,0 +1,5 @@
+ const Rims = () => {
+    return <h1>RIMS Suite of apps</h1>
+}
+
+export default Rims;
