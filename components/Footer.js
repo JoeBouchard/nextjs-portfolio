7ae91b7f694a305a82ed35ecfe3527bc97c3604d
Joe Bouchard <joseph.c.bouchard-1@ou.edu>
2022-07-03 10:04:30 -0500

    Created header and footer components

---
+++ components/Footer.js
@@ -0,0 +1,22 @@
+import { TbBrandNextjs as Next } from "react-icons/tb";
+import { FaReact } from "react-icons/fa";
+import { SiTailwindcss as Tailwind, SiVercel as Vercel } from "react-icons/si";
+
+const Footer = () => {
+  return (
+    <footer className="flex flex-grow text-zinc-600 dark:text-gray-400 mt-5">
+      <nav className="text-md bg-white dark:bg-black p-1 shadow-md w-full h-fill bottom-0 text-center mt-0">
+        <h3 className="text-center ">
+          Built with:{"  "}
+          <FaReact className="inline" /> React, <Next className="inline" />{" "}
+          NextJS, and <Tailwind className="inline" /> Tailwind
+        </h3>
+        <h3 className="my-1">
+          &copy; Joseph Bouchard, {new Date().getFullYear()}
+        </h3>
+      </nav>
+    </footer>
+  );
+};
+
+export default Footer;
--- pages/_app.js
+++ pages/_app.js
@@ -1,58 +1,15 @@
 import "../styles/globals.css";
-import { BsMoon as Moon, BsSun as Sun } from "react-icons/bs";
-import { TbBrandNextjs as Next } from "react-icons/tb";
-import { FaReact } from "react-icons/fa";
-import { SiTailwindcss as Tailwind, SiVercel as Vercel } from "react-icons/si";
-import React, { useEffect, useReducer } from "react";
-import Link from "next/link";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const MyApp = ({ Component, pageProps }) => {
-  const [dark, toggle] = useReducer((d) => !d, true);
-
-  useEffect(() => {
-    localStorage.theme = dark ? "dark" : "light";
-    console.log(dark, localStorage.theme);
-    if (dark) document.documentElement.classList.add("dark");
-    else document.documentElement.classList.remove("dark");
-  }, [dark]);
-
   return (
     <div
-      className={`flex flex-col bg-zinc-100 h-full md:h-screen dark:bg-zinc-900 text-zinc-700 dark:text-slate-300`}
+      className={`flex flex-col bg-zinc-100 dark:bg-zinc-900 text-zinc-700 dark:text-slate-300`}
     >
-      <header className="mb-5">
-        <nav className="navbar navbar-expand-lg shadow-md py-2 bg-white dark:bg-black items-center w-full justify-between">
-          <button
-            className="nav-item p-3 text-lg"
-            onClick={() => {
-              toggle();
-              console.log("clicked");
-            }}
-          >
-            {dark ? <Sun className="text-white" /> : <Moon />}
-          </button>
-          <Link
-            class="nav-item p-3 text-lg"
-            id="navbarSupportedContentY"
-            href="/"
-          >
-            Home
-          </Link>
-        </nav>
-      </header>
+      <Header />
       <Component {...pageProps} />
-      <footer className="flex flex-grow">
-        <nav className="text-md bg-white dark:bg-black p-1 shadow-md w-full h-fill bottom-0 text-center mt-0">
-          <h3 className="text-center ">
-            Built with:{"  "}
-            <FaReact className="inline" /> React, <Next className="inline" />{" "}
-            NextJS, and <Tailwind className="inline" /> Tailwind
-          </h3>
-          <h3 className="my-1">
-            &copy; Joseph Bouchard, {new Date().getFullYear()}
-          </h3>
-        </nav>
-      </footer>
+      <Footer />
     </div>
   );
 };
