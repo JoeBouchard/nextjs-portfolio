d6edb3b4cd50e6ee2b68c29b33360b84bd44c5b3
Joe Bouchard <joseph.c.bouchard-1@ou.edu>
2022-07-04 21:33:24 -0500

    MAde badges transparent

--- components/Badge.js
+++ components/Badge.js
@@ -1,5 +1,5 @@
 const Badge = ({ children }) => (
-  <div className="content-center m-2 p-2 rounded dark:bg-slate-200 bg-zinc-700 dark:text-zinc-800 text-slate-300 text-xl text-center font-bold border border-stone-500 border-2 shadow-lg">
+  <div className="content-center m-2 p-2 rounded text-xl text-center font-bold border border-stone-500 border-2 shadow-lg">
     {children}
   </div>
 );
