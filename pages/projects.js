2a96a16cf24cf3f5c4f4e622b2bb89ab5216916f
Joe Bouchard <joseph.c.bouchard-1@ou.edu>
2022-09-02 14:06:58 -0500

    Added projects page

---
+++ pages/projects.js
@@ -0,0 +1,130 @@
+import { useState } from "react";
+
+const Projects = () => {
+  const [selected, setSelected] = useState();
+
+  const BoxStyle = ({ children, name }) => {
+    if (selected && selected !== name) {
+      return <></>;
+    }
+
+    return (
+      <div
+        style={{
+          overflow: "hidden",
+          textOverflow: "ellipsis",
+          transformOrigin: "0% 0%",
+          cursor: "pointer",
+        }}
+        className={`${
+          selected === name ? "animate-expand" : "four-line"
+        } mx-3 p-2 border rounded-lg grow border-zinc-700 dark:border-zinc-300 border-3 `}
+        onClick={() => setSelected(selected === name ? undefined : name)}
+      >
+        {children}
+      </div>
+    );
+  };
+
+  return (
+    <div className="text-center">
+      <h2 className="text-4xl my-2">Projects</h2>
+      <div className={`${!selected && "grid grid-cols-2 "} mt-10`}>
+        <BoxStyle name="tascs">
+          <h2 className="text-2xl font-bold ">
+            Tickets And Service Center Systems (TASCS)
+          </h2>
+          <div>
+            <span>
+              A system designed to manage service requests for the Oklahoma
+              Department of Public Safety.
+            </span>
+            <h4 className="text-lg text-bold mt-2">Primary functions:</h4>
+            <ul>
+              <li>
+                Receive requests from a Sharepoint form and notify technicians
+              </li>
+              <li>Allow technicians to view and interact with request data</li>
+              <li>
+                Allow technicians to contact requestors and set up appointments
+              </li>
+              <li>
+                Alert technicians of upcoming appontments, walk in requests, and
+                tasks to work on
+              </li>
+            </ul>
+            <h4 className="text-lg text-bold mt-2">More details:</h4>
+            <p>
+              Users submit a request using a SharePoint form, which then sends
+              the request to a backend MySql database via a Python Flask API. A
+              website written in ReactJS uses the Python API to allow
+              technicians to claim and work on tickets, schedule appointments,
+              and view all data regarding the ticket. Actions taken on the
+              ticket, from creation to completion, are emailed to the relevant
+              parties. In addition, there is a kiosk running on a Chromebox that
+              allows users to register for a walk in appointment or check in for
+              a scheduled appointment. Finally, there is a technician kiosk that
+              shows the status of all open tickets and upcoming meetings and
+              alerts technicians of a new ticket or a check in with flashing
+              lights and sound.
+            </p>
+          </div>
+        </BoxStyle>
+        <BoxStyle name="magic">
+          <h2 className="text-2xl font-bold ">
+            Multi Application Geographic Information Center (MAGIC)
+          </h2>
+          <div>
+            <span>
+              A system designed to monitor the status of various DPS and ODOT
+              owned services across the state.
+            </span>
+            <h4 className="text-lg text-bold mt-2">Primary functions:</h4>
+            <ul>
+              <li>
+                Display live status of 50+ radio towers across the state on a
+                map and send emails when one has an issue
+              </li>
+              <li>
+                Show vital signs (% functionality, fuel levels, hardware status)
+                for these towers
+              </li>
+              <li>
+                Show radio network connections between towers, color coded for
+                connection type
+              </li>
+              <li>
+                Indicate which radio towers are due for routine maintenance
+              </li>
+              <li>
+                Show real-time position, speed, and heading for civilian and
+                trooper vehicles
+              </li>
+              <li>
+                Display live connection status of access control hardware
+                statewide
+              </li>
+              <li>
+                Display up-to-date weather and temperature across the state
+              </li>
+              <li>Show all active wildfires across the state</li>
+            </ul>
+            <h4 className="text-lg text-bold mt-2">More details:</h4>
+            <span>
+              Interfaces with the OKWIN and PatrolNet radio networks, our
+              AMAG/Symmetry access control database, the IRWIN fire data API,
+              the Oklahoma Mesonet, the Rainviewer API, OpenStreetMaps, and our
+              in-house MySql server to display up to date conditions on the
+              state of the State's assets. Using Leaflet in ReactJS, all data is
+              shown on a map of the state, with each data layer being toggleable
+              and customizable to show only the relevant information the user
+              desires.
+            </span>
+          </div>
+        </BoxStyle>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
       animation: {
         fadeUp: "fadeUp 0.5s cubic-bezier(.19,1.06,.75,1.55)",
         grow: "grow 0.25s cubic-bezier(.19,1.06,.75,1.55) forwards",
+        expand: "expand 1s cubic-bezier(.12,.48,.37,.71)",
       },
       keyframes: {
         fadeUp: {
@@ -25,6 +26,11 @@ module.exports = {
         grow: {
           "0%": { transform: "scale(1, 1)" },
           "100%": { transform: "scale(1.05, 1.05)" },
+        },
+        expand: {
+          "0%": { transform: "scale(0.25, 0.25) ", filter: "blur(10px)" },
+          "50%": { transform: "scale(1, 0.25)  ", filter: "blur(10px)" },
+          "100%": { transform: "scale(1, 1)  ", filter: "blur(0px)" },
         },
       },
     },
