3df4c21bafee956f55c1322967e0862f7b0d863d
Joe Bouchard <joseph.c.bouchard-1@ou.edu>
2022-09-06 00:20:55 -0500

    Improved animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        fadeUp: "fadeUp 0.5s cubic-bezier(.19,1.06,.75,1.55)",
+        fadeUp: "fadeUp 0.5s cubic-bezier(.19,1.06,.75,1.55) backwards",
         grow: "grow 0.25s cubic-bezier(.19,1.06,.75,1.55) forwards",
         expand: "expand 1s cubic-bezier(.12,.48,.37,.71)",
       },
@@ -16,7 +16,7 @@ module.exports = {
         fadeUp: {
           "0%": {
             transform: "scale(0.1, 0.1) translateY(50px)",
-            opacity: 0.5,
+            opacity: 0,
           },
           "100%": {
             transform: "scale(1, 1) translateY(0px)",
@@ -32,6 +32,9 @@ module.exports = {
           "50%": { transform: "scale(1, 0.25)  ", filter: "blur(10px)" },
           "100%": { transform: "scale(1, 1)  ", filter: "blur(0px)" },
         },
+      },
+      fontFamily: {
+        "uncial-antiqua": ['"Uncial Antiqua"', "cursive"],
       },
     },
   },
